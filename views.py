dc58fbbab1e1b347505ca5565c698b60e837e34e
Raj <rsarkar@alumni.utexas.net>
2023-12-08 12:58:02 -0500

    I think this should be all the different views that we could use.
    * SetList, SetDetail
    * TypeList
    * SubtypeList
    * SupertypeList
    * FormatList

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from mtgsdk.card import Card
+from mtgsdk.card import Card, Format, Set, Subtype, Supertype, Type
 
 from rest_framework import generics
 from rest_framework.response import Response
@@ -6,11 +6,36 @@ from rest_framework.views import APIView
 
 
 class CardList(APIView):
-  """
-  View to list all Card instances.
-  """
-  def get(self, request, format=None):
-    return Card.all()
+    """
+    View to list all Card instances.
+    """
+    def get(self, request, format=None):
+        return Card.all()
+
 
 class CardDetail(generics.GenericAPIView):
-  pass
+    pass
+
+
+class SetList(APIView):
+    pass
+
+
+class SetDetail(generics.GenericAPIView):
+    pass
+
+
+class TypeList(APIView):
+    pass
+
+
+class SubtypeList(APIView):
+    pass
+
+
+class SupertypeList(APIView):
+    pass
+
+
+class FormatList(APIView):
+    pass
